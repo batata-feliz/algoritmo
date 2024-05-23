290a07912e643c302186e7ab18516f660d13a792
batata-feliz <166560229+batata-feliz@users.noreply.github.com>
2024-05-23 11:57:15 -0300

    aulas sobre operadores logicos

---
+++ java script/main.js
@@ -0,0 +1,13 @@
+
+var numero10 = 
+
+console.log("10x1 =",numero10*1)
+console.log("10x2 =",numero10*2)
+console.log("10x3 =",numero10*3)
+console.log("10x4 =",numero10*4)
+console.log("10x5 =",numero10*5)
+console.log("10x6 =",numero10*6)
+console.log("10x7 =",numero10*7)
+console.log("10x8 =",numero10*8)
+console.log("10x9 =",numero10*9)
+console.log("10x10 =",numero10*10)
