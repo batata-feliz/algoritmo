055736f2b7d26d2dbeefddf18e55c14e9ded4cfd
batata-feliz <166560229+batata-feliz@users.noreply.github.com>
2024-05-24 09:47:56 -0300

    atividade 01 a 05

--- java script/main.js
+++ java script/main.js
@@ -1,13 +1,44 @@
+var numero1,numero2
 
-var numero10 = 
+numero1 = 5
+numero2 = 3
+console.log("ATIVIDADE 01")
+console.log("=========")
+console.log(numero1 + numero2)
+console.log(numero1 - numero2)
+console.log(numero1 * numero2)
+console.log(numero1 / numero2)
+console.log("")
+console.log("")
 
-console.log("10x1 =",numero10*1)
-console.log("10x2 =",numero10*2)
-console.log("10x3 =",numero10*3)
-console.log("10x4 =",numero10*4)
-console.log("10x5 =",numero10*5)
-console.log("10x6 =",numero10*6)
-console.log("10x7 =",numero10*7)
-console.log("10x8 =",numero10*8)
-console.log("10x9 =",numero10*9)
-console.log("10x10 =",numero10*10)
+
+console.log("ATIVIDADE 02/03")
+console.log("============")
+var celsius,Fahrenheit
+
+Fahrenheit = 120
+celsius = 30.2
+ 
+var valordeFahrenheit = Fahrenheit-32
+var valordecelsius = celsius*1.8
+
+console.log("ATIVIDADE 2",valordeFahrenheit/1.8)
+console.log("ATIVIDADE 3",valordecelsius+32)
+console.log("")
+console.log("")
+
+console.log("ATIVIDADE 04")
+var metros
+console.log("============")
+metros = 2
+
+console.log("metros em centimetros são =",metros*100)
+console.log("")
+console.log("")
+
+console.log("ATIVIDADE 05")
+console.log("============")
+var preco = 500
+var desconto = preco*0.1
+var precofinal = preco - desconto
+console.log("o valor com desconto será R$"+precofinal)
