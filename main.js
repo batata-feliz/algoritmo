0f47d3274997e529327f59e1b5d1ae25e6da3b0a
batata-feliz <166560229+batata-feliz@users.noreply.github.com>
2024-05-24 09:51:05 -0300

    trabalho

---
+++ main.js
@@ -0,0 +1,44 @@
+var numero1,numero2
+
+numero1 = 5
+numero2 = 3
+console.log("ATIVIDADE 01")
+console.log("=========")
+console.log(numero1 + numero2)
+console.log(numero1 - numero2)
+console.log(numero1 * numero2)
+console.log(numero1 / numero2)
+console.log("")
+console.log("")
+
+
+console.log("ATIVIDADE 02/03")
+console.log("============")
+var celsius,Fahrenheit
+
+Fahrenheit = 120
+celsius = 30.2
+ 
+var valordeFahrenheit = Fahrenheit-32
+var valordecelsius = celsius*1.8
+
+console.log("ATIVIDADE 2",valordeFahrenheit/1.8)
+console.log("ATIVIDADE 3",valordecelsius+32)
+console.log("")
+console.log("")
+
+console.log("ATIVIDADE 04")
+var metros
+console.log("============")
+metros = 2
+
+console.log("metros em centimetros são =",metros*100)
+console.log("")
+console.log("")
+
+console.log("ATIVIDADE 05")
+console.log("============")
+var preco = 500
+var desconto = preco*0.1
+var precofinal = preco - desconto
+console.log("o valor com desconto será R$"+precofinal)
